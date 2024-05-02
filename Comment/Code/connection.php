b07e7c2712237b79f431d2f1de2758cb6705872f
Zane Daniel <118499857+ZaneDaniel11@users.noreply.github.com>
2024-05-02 11:49:00 +0800

    Comment
    Comment

---
+++ Comment/Code/connection.php
@@ -0,0 +1,11 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$data_base = 'blog';
+
+$conn = mysqli_connect($host, $username, $password, $data_base);
+
+if ($conn) {
+    // echo'Connected Succesfully';
+}
